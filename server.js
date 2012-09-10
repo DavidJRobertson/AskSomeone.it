eafec422eb9df2588efe8ce1cb05f4329033e609
David Robertson <david@davidr.me>
2012-09-10 03:05:28 +0100

    Improved node console logging and fixed queue worker timer.

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ io.sockets.on('connection', function (socket){
     try {
     userid = sanitize(id).toInt();
     check(userid).len(5, 20);
-    console.log("New connection from user with ID " + userid);
+    console.log("New connection from user " + userid);
     } catch (e) {
       socket.emit("error", "An internal error has occurred :( Please refresh the page and try again.");
     }
@@ -52,44 +52,42 @@ io.sockets.on('connection', function (socket){
   socket.on("idrequest", function(){
     userid = Math.floor(Math.random() * 324135143); // TODO: make this better
     socket.emit("id", userid);
+    console.log("New ID request, issued id = " + userid);
   });
   
   socket.on("question", function (data){
     try {
-    var qtext = sanitize(data).xss();
-    check(qtext).len(5, 100).notEmpty().notContains("http://").notContains("https://");
-    rc.lpush("questionqueue", JSON.stringify({"text": qtext, "asker": {"userid": userid, "socketid": socket.id}}));
+      var qtext = sanitize(data).xss();
+      check(qtext).len(5, 100).notEmpty().notContains("http://").notContains("https://");
+      rc.lpush("questionqueue", JSON.stringify({"text": qtext, "asker": {"userid": userid, "socketid": socket.id}}));
+      console.log("Quesion received from user " + userid + " question = " + qtext);
     } catch (e) {
+      console.log("Invalid question submitted by user " + userid + ", question = " + qtext);
       socket.emit("error", "That doesn't look like a valid question to us. Refresh the page and try another :P");
     }
   });
   
   socket.on("answer", function(qapair) {
     qa = JSON.parse(qapair);
-    
-    
-    console.log("Recieved question/answer pair");
-    console.log(qapair);
-    
-    
-    
+  
+    console.log("Received question/answer pair: " + qapair);
+      
     io.sockets.sockets[qa.question.asker.socketid].emit("answer", qapair);
   });
 });
 
 
-setInterval(processQueue, 3000);
+setInterval(processQueue, 300);
 
 function processQueue(){
   try {
   rc.llen("questionqueue", function(error, length){
-    console.log("Queue Length: " + length);
     if (length > 1) {
-      console.log("PAIRING!");
       rc.rpop("questionqueue", function(error, q1r){
         rc.rpop("questionqueue", function(error, q2r){
           q1 = JSON.parse(q1r);
           q2 = JSON.parse(q2r);
+          console.log("Pairing user " + q1.asker.userid + " with user " + q2.asker.userid);
           io.sockets.sockets[q1.asker.socketid].emit("question", q2r);
           io.sockets.sockets[q2.asker.socketid].emit("question", q1r);
         });
