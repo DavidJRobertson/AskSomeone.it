d9bdfe591121e9279831e0813b8caebf3d73dee2
David Robertson <david@davidr.me>
2012-09-13 18:03:57 +0100

    No 404 page any more, just redirect to /

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var redis  = require("redis"),
-    rc     = redis.createClient();
-var app    = require("http").createServer(handler);
-var io     = require("socket.io").listen(app);
-var check = require('validator').check,
+var redis    = require("redis"),
+    rc       = redis.createClient();
+var app      = require("http").createServer(handler);
+var io       = require("socket.io").listen(app);
+var check    = require('validator').check,
     sanitize = require('validator').sanitize;
-var static = require("node-static");
-var util   = require("util");
+var static   = require("node-static");
+var util     = require("util");
+var fs       = require("fs");
 
-var webroot = 'static';
-var file = new(static.Server)(webroot, {
+
+var file = new(static.Server)('static', {
   cache: 600
 });
 
@@ -34,7 +35,9 @@ function handler (req, res){
       if (err) {
         //console.error('Error serving %s - %s', req.url, err.message);
         if (err.status === 404 || err.status === 500) {
-          file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+          res.writeHead(302, {'Location': '/'});
+          res.end();
+          //file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
         } else {
           res.writeHead(err.status, err.headers);
           res.end();
