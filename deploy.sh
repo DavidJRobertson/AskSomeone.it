b4e1e1671ff4ea32806566f10f8a7ce16e21f637
David Robertson <david@davidr.me>
2012-10-17 04:46:11 +0100

    Fiddling with deploy script... Now you can also run it server side with ./deploy.sh production master server-deploy

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,21 @@ REF=${2:-"master"}
 trap 'test -n "$SUCCESS" || echo "  error: aborted"' EXIT
 echo "* Deploying $ENVIRONMENT/$REF"
 
-ssh $USER@$SERVER "cd $DEPLOY_PATH && \
-                   git reset --hard --quiet && \
-                   git checkout --quiet $REF && \
-                   git pull && \
-                   npm install && \
-                   sudo /etc/init.d/asksomeone restart"
 
+
+COMMAND="cd $DEPLOY_PATH && \
+         git reset --hard --quiet && \
+         git checkout --quiet $REF && \
+         git pull && \
+         npm install && \
+         sudo /etc/init.d/asksomeone restart"
+
+
+
+if [[ "$3" == "server-deploy" ]]
+then
+    eval $COMMAND
+else
+    ssh $USER@$SERVER "$COMMAND"
+fi
 SUCCESS=true
